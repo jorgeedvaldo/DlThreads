32ea3015bbd27852261caf205b869936727febab
Edivaldo Jorge <32965474+jorgeedvaldo@users.noreply.github.com>
2023-07-11 17:33:18 +0000

    Define parameters

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.send('Hello, World! ' + req.query.postid);
   });
 
 //starting the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`API server listening on port ${port}`);
-});
+    console.log(`API server listening on port ${port}`);
+  });
