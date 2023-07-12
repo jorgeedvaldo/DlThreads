041d2793646f8765aac198e9714557c8d567967c
Edivaldo Jorge <32965474+jorgeedvaldo@users.noreply.github.com>
2023-07-12 13:58:52 +0000

    tests with json objects

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
 const express = require('express');
-const app = express();
+const puppeteer = require('puppeteer');
 
+let FinalUrl = '';
+//Configure Puppeteer
+(async () => {
+  // Inicializar o Chromium
+  const browser = await puppeteer.launch();
+
+  // Abrir uma nova página
+  const page = await browser.newPage();
+
+  // Navegar para o website desconhecido
+  const websiteUrl = 'https://www.threads.net/t/Cug7zq2AfVB/';
+  await page.goto(websiteUrl);
+  await page.waitForSelector('.x1xmf6yo img');
+
+  const imgElements = await page.$$('img');
+  if (imgElements.length >= 1) {
+    const secondImgSrc = await page.evaluate((img) => img.src, imgElements[3]);
+    console.log('Src do segundo elemento <img>: ', secondImgSrc);
+    FinalUrl = secondImgSrc;
+  } else {
+    console.log('Não há segundo elemento <img> na página.');
+  }
+  
+
+  // Fechar o Chromium
+  await browser.close();
+})();
+
+
+//Configure express
+const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello, World! ' + req.query.postid);
+    //res.send(secondImgSrc + ' Hello, World! ' + req.query.postid);
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ url: FinalUrl }));
   });
 
 //starting the server
