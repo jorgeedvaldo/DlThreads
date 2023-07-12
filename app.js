f63e508d60762f9f8e10b4684f9e8c929933898d
Edivaldo Jorge <32965474+jorgeedvaldo@users.noreply.github.com>
2023-07-12 16:37:12 +0100

    returning the post image link

--- app.js
+++ app.js
@@ -1,41 +1,55 @@
 const express = require('express');
 const puppeteer = require('puppeteer');
 
-let FinalUrl = '';
-//Configure Puppeteer
-(async () => {
-  // Inicializar o Chromium
-  const browser = await puppeteer.launch();
-
-  // Abrir uma nova página
-  const page = await browser.newPage();
-
-  // Navegar para o website desconhecido
-  const websiteUrl = 'https://www.threads.net/t/Cug7zq2AfVB/';
-  await page.goto(websiteUrl);
-  await page.waitForSelector('.x1xmf6yo img');
-
-  const imgElements = await page.$$('img');
-  if (imgElements.length >= 1) {
-    const secondImgSrc = await page.evaluate((img) => img.src, imgElements[3]);
-    console.log('Src do segundo elemento <img>: ', secondImgSrc);
-    FinalUrl = secondImgSrc;
-  } else {
-    console.log('Não há segundo elemento <img> na página.');
-  }
-  
-
-  // Fechar o Chromium
-  await browser.close();
-})();
-
 
 //Configure express
 const app = express();
 app.get('/', (req, res) => {
+
+    let FinalUrl = '';
+    //Configure Puppeteer
+    (async () => {
+      // Inicializar o Chromium
+      const browser = await puppeteer.launch();
+    
+      // Abrir uma nova página
+      const page = await browser.newPage();
+    
+      // Navegar para o website desconhecido
+      const websiteUrl = 'https://www.threads.net/t/' + req.query.postid;
+      await page.goto(websiteUrl);
+      
+      try {
+        await page.waitForSelector('.x1xmf6yo img');
+      } catch (error) {
+        console.error(error);
+        // Expected output: ReferenceError: nonExistentFunction is not defined
+        // (Note: the exact output may be browser-dependent)
+      }
+      
+    
+      const imgElements = await page.$$('img');
+      if (imgElements.length >= 1) {
+        const secondImgSrc = await page.evaluate((img) => img.src, imgElements[3]);
+        console.log('Src do segundo elemento <img>: ', secondImgSrc);
+        FinalUrl = secondImgSrc;
+        //res.send(secondImgSrc + ' Hello, World! ' + req.query.postid);
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({ url: FinalUrl }));
+      } else {
+        console.log('Não há segundo elemento <img> na página.');
+      }
+      
+    
+      // Fechar o Chromium
+      await browser.close();
+    })();
+
+
+    //LOCAL DO RETORNO DO JSON
     //res.send(secondImgSrc + ' Hello, World! ' + req.query.postid);
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({ url: FinalUrl }));
+    //res.setHeader('Content-Type', 'application/json');
+    //res.end(JSON.stringify({ url: FinalUrl }));
   });
 
 //starting the server
